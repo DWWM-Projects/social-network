349d035766e5fbf8e65f534304682730778bee24
Antrâxx <h.julien62@gmail.com>
2022-09-08 17:09:47 +0200

    Entity fixes, Login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUserName();
+
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'error' => $error,
+            'last_username' => $lastUsername,
         ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout()
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -7,9 +7,18 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
@@ -21,6 +30,7 @@ class AppFixtures extends Fixture
             $user->setEmail($faker->email());
             $user->setBiography($faker->text());
             $user->setBirthdate($faker->dateTimeBetween('-1 weeks', '-1 days'));
+            $user->setPassword($this->hasher->hashPassword($user, 'password'));
             $manager->persist($user);
             $this->addReference('user-'.$i, $user);
         }

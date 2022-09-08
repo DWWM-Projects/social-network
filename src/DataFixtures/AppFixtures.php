31789e2a7d90dad6aa78ad723b27d066ba849bb9
Antrâxx <h.julien62@gmail.com>
2022-09-08 11:28:51 +0200

    Fixtures User-Publications

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Publication;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -13,14 +14,23 @@ class AppFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
-        for ($i = 0; $i <= 10; $i++) {
+        for ($i = 1; $i <= 10; $i++) {
             $user = new User();
             $user->setName($faker->name());
             $user->setPseudo($faker->word());
             $user->setEmail($faker->email());
             $user->setBiography($faker->text());
-            $user->setBirthdate($faker->dateTime());
+            $user->setBirthdate($faker->dateTimeBetween('-1 weeks', '-1 days'));
             $manager->persist($user);
+            $this->addReference('user-'.$i, $user);
+        }
+
+        for ($i = 0; $i <= 40; $i++) {
+            $publication = new Publication();
+            $publication->setContent($faker->text());
+            $publication->setCreatedAt($faker->dateTimeBetween('-1 days', '-1hours'));
+            $publication->setUser($this->getReference('user-'.rand(1, 10)));
+            $manager->persist($publication);
         }
 
 
